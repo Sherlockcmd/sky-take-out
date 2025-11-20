7de8464613e9e77d51b2d255679513ab8d437275
Sherlockcmd <921277132@qq.com>
2025-11-20 21:06:14 +0800

    完善工作台相关代码，项目初步完成

---
+++ sky-server/src/main/java/com/sky/service/impl/WorkspaceServiceImpl.java
@@ -0,0 +1,142 @@
+package com.sky.service.impl;
+
+import com.sky.constant.StatusConstant;
+import com.sky.entity.Orders;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.OrderMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.UserMapper;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+import net.sf.jsqlparser.expression.DateTimeLiteralExpression;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class WorkspaceServiceImpl implements WorkspaceService {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Override
+    public BusinessDataVO businessData(LocalDateTime begin,LocalDateTime end) {
+
+        /**
+         * 营业额：当日已完成订单的总金额
+         * 有效订单：当日已完成订单的数量
+         * 订单完成率：有效订单数 / 总订单数
+         * 平均客单价：营业额 / 有效订单数
+         * 新增用户：当日新增用户的数量
+         */
+
+        Map map = new HashMap();
+
+        map.put("begin",begin);
+        map.put("end",end);
+
+        Integer OrderCount = orderMapper.countByMap(map);
+
+        map.put("status", Orders.COMPLETED);
+        Integer validOrderCount = orderMapper.countByMap(map);
+        validOrderCount = validOrderCount==null ? 0 :validOrderCount;
+        Double  turnover = orderMapper.sumByMap(map);
+        turnover = turnover==null ? 0.0 :turnover;
+        Double  orderCompletionRate = (1.0*validOrderCount/OrderCount);
+        orderCompletionRate = orderCompletionRate==null ? 0.0 :orderCompletionRate;
+        Integer newUser = userMapper.sumUserByMap(map);
+
+        Double  unitPrice = turnover/validOrderCount;
+
+        newUser = newUser==null ? 0 :newUser;
+
+        unitPrice = unitPrice==null ? 0.0 :unitPrice;
+
+
+        return BusinessDataVO.builder()
+                .newUsers(newUser)
+                .orderCompletionRate(orderCompletionRate)
+                .turnover(turnover)
+                .validOrderCount(validOrderCount)
+                .unitPrice(unitPrice)
+                .build();
+    }
+
+    @Override
+    public OrderOverViewVO getOrderOverView() {
+        Map map = new HashMap();
+        map.put("begin", LocalDateTime.now().with(LocalTime.MIN));
+        map.put("status", Orders.TO_BE_CONFIRMED);
+
+        //待接单
+        Integer waitingOrders = orderMapper.countByMap(map);
+
+        //待派送
+        map.put("status", Orders.CONFIRMED);
+        Integer deliveredOrders = orderMapper.countByMap(map);
+
+        //已完成
+        map.put("status", Orders.COMPLETED);
+        Integer completedOrders = orderMapper.countByMap(map);
+
+        //已取消
+        map.put("status", Orders.CANCELLED);
+        Integer cancelledOrders = orderMapper.countByMap(map);
+
+        //全部订单
+        map.put("status", null);
+        Integer allOrders = orderMapper.countByMap(map);
+
+        return OrderOverViewVO.builder()
+                .waitingOrders(waitingOrders)
+                .deliveredOrders(deliveredOrders)
+                .completedOrders(completedOrders)
+                .cancelledOrders(cancelledOrders)
+                .allOrders(allOrders)
+                .build();
+    }
+
+    @Override
+    public DishOverViewVO getDishOverView() {
+        Map map = new HashMap();
+        map.put("status", StatusConstant.ENABLE);
+        Integer sold = dishMapper.countByMap(map);
+
+        map.put("status", StatusConstant.DISABLE);
+        Integer discontinued = dishMapper.countByMap(map);
+
+        return DishOverViewVO.builder()
+                .sold(sold)
+                .discontinued(discontinued)
+                .build();
+    }
+
+    @Override
+    public SetmealOverViewVO getSetmealOverView() {
+        Map map = new HashMap();
+        map.put("status", StatusConstant.ENABLE);
+        Integer sold = setmealMapper.countByMap(map);
+
+        map.put("status", StatusConstant.DISABLE);
+        Integer discontinued = setmealMapper.countByMap(map);
+
+        return SetmealOverViewVO.builder()
+                .sold(sold)
+                .discontinued(discontinued)
+                .build();
+    }
+}
